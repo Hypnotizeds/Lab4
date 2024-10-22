6dfd857fd8041fb05389c3a68d670375b505aa73
Hypnotizeds <82241662+Hypnotizeds@users.noreply.github.com>
2024-10-22 16:43:22 +0300

    Create Перемещение метода.py

---
+++ Перемещение метода.py
@@ -0,0 +1,8 @@
+class Order:
+    def calculate_total(self):
+        # Логика
+
+# Если метод связан с классом Payment, переместите его туда
+class Payment:
+    def calculate_total(self):
+        # Логика
